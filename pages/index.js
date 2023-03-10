c830deba74c65b1a8fb841169ce75662f77b031c
Paulo Bezerra <bezerra.paulo@gmail.com>
2023-03-10 20:03:45 +0000

    First commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return <div>
+      <h1 style={{color: 'red'}}>Hagrayzs! Urgente!</h1>
+      <p>Você deveria voltar para o seu curso de python!</p>
+      <a href="https://youtu.be/S9uPNppGsGo">Clique Aqui</a>
+    </div>
+}
+
+export default Home;
