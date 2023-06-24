88e729de14db971887f26a7e408f281c3d3c6f8d
Paulo Bezerra <bezerra.paulo@gmail.com>
2023-06-24 19:59:30 +0000

    adicionando scripts `lint:chack` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 function Home() {
-  return <div>
-    <h1 style={{ color: 'red' }}>Hagrayzs! Urgente!</h1>
-    <p>Você deveria voltar para o seu curso de python!</p>
-    <a href="https://youtu.be/S9uPNppGsGo">Clique Aqui</a>
-  </div>
-}  
+  return (
+    <div>
+      <h1 style={{ color: "red" }}>Hagrayzs! Urgente!</h1>
+      <p>Você deveria voltar para o seu curso de python!</p>
+      <a href="https://youtu.be/S9uPNppGsGo">Clique Aqui</a>
+    </div>
+  );
+}
 
 export default Home;
